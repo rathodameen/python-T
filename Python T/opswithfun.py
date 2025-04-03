efdf292b4eceb0e930349b15f1878208cb01b7e0
Rathod Ameen <131616244+rathodameen@users.noreply.github.com>
2025-04-03 19:06:10 +0530

    Create opswithfun.py

---
+++ Python T/opswithfun.py
@@ -0,0 +1,21 @@
+def add(a,b): #addition
+    print(a+b)
+a = input()
+b = input()
+add(a,b)
+
+def greet(c): #greeting
+    print("welcome "+ c)
+greet("rathod")
+
+def sqr(d): #square
+    print(d*d)
+sqr(21)
+
+def mul(i,j): #multiply
+    print(i*j)
+mul(2,5)
+
+def str1(m): #str length
+    print(len(m))
+str1("rathodameen")
