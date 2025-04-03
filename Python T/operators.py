fcba2c6cec8d9646f2bb8e40f99d8738d0928a57
Rathod Ameen <131616244+rathodameen@users.noreply.github.com>
2025-04-03 19:02:29 +0530

    Create operators.py

---
+++ Python T/operators.py
@@ -0,0 +1,22 @@
+#OPERATORS IN PYTHON
+a=10
+b=15
+sum=a+b
+sub=a-b
+mul=a*b
+div=a/b
+exp=a**b
+diiv=a//b
+print(sum,sub,mul,div,exp,diiv)
+
+
+dic = {'name':'ameen','city':'HYD','course':'Python FST'}
+print('city' in dic)
+
+x=e
+print(x in a)
+
+
+def samole(a):
+    print("hii,+a)
+samole("een")
