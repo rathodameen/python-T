edd2a4ecca093d61b22312f04da41d3ee29bf19a
Rathod Ameen <131616244+rathodameen@users.noreply.github.com>
2025-04-22 10:42:18 +0530

    Create 13-03-25.py

---
+++ Python T/13-03-25.py
@@ -0,0 +1,31 @@
+
+# task
+month = int(input("Enter the value"))
+
+match month:
+    case 0:
+        print("Jan")
+    case 1:
+        print("Feb")
+    case 2:
+        print("March")
+    case 3:
+        print("Apeil")
+    case 4:
+        print("May")
+    case 5:
+        print("June")
+    case 6:
+        print("July")
+    case 7:
+        print("Aug")
+    case 8:
+        print("Sep")
+    case 9:
+        print("Oct")
+    case 10:
+        print("Nov")
+    case 11:
+        print("Dec")
+    case _:
+        print("wrong case")
